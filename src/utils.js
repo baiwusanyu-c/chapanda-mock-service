b9a3b4e724981d76c72ea716a5b48dd535847908
baiwusanyu-c <chenzhihui@chabaidao.com>
2024-09-12 09:42:53 +0800

    Initial commit

---
+++ src/utils.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+
+const METHODS_MAP = {
+  POST: 'post',
+  GET: 'get',
+  DELETE: 'delete',
+  PUT: 'put',
+}
+
+const createRoutes = (router, p) => {
+  const stats = fs.statSync(p)
+
+  const handleResult = (router, result) => {
+    const { path, methods, data } = result
+    if (!methods)
+      console.error(
+        `Methods did not exist. Please make sure your method is one of ${Object.keys(
+          METHODS_MAP,
+        ).join(' ')}`,
+      )
+
+    if (!data)
+      console.error(
+        `Data did not exists. Please make sure your data is correct`,
+      )
+
+    router[METHODS_MAP[`${methods}`]](path, (req, res) => {
+      if (typeof data === 'function') {
+        res.json(data(req))
+      } else {
+        res.json(data)
+      }
+    })
+  }
+
+  if (stats.isDirectory()) {
+    fs.readdirSync(p).forEach((item) => {
+      createRoutes(router, `${p}/${item}`)
+    })
+  } else if (stats.isFile()) {
+    const results = require(`${p}`)
+    if (Object.prototype.toString.call(results) === '[object Object]') {
+      handleResult(router, results)
+    } else if (Object.prototype.toString.call(results) === '[object Array]') {
+      results.forEach((result) => {
+        handleResult(router, result)
+      })
+    } else {
+      throw new Error(`Mock File Content Error...`)
+    }
+  }
+}
+
+module.exports = {
+  createRoutes,
+}
